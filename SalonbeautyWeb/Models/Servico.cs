cc5db16c733d67e11f649c43aa9d80d2e62c77a3
Cesar Filho <cesar.pc.filho@gmail.com>
2016-08-22 23:54:52 -0300

    primeiro commit cesar

---
+++ SalonbeautyWeb/Models/Servico.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SalonbeautyWeb.Models
+{
+    public class Servico
+    {
+        public DateTime DatMarcada { get; set; }
+
+    }
+}
